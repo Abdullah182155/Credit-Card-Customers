185725543a75a161b462e8077de848cfc0b8d9e0
Abdullah Ashraf <Abdullah182155@gmail.com>
2025-03-17 01:29:33 +0200

    Using DecisionTree

--- script.py
+++ script.py
@@ -10,6 +10,7 @@ from sklearn.preprocessing import OneHotEncoder, StandardScaler
 from sklearn.pipeline import Pipeline, FeatureUnion
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import f1_score, confusion_matrix
 from imblearn.over_sampling import SMOTE
 from sklearn_features.transformers import DataFrameSelector
@@ -108,7 +109,7 @@ def train_model(X_train, y_train, plot_name='', class_weight=None):
     
     global clf_name
 
-    clf = LogisticRegression(class_weight=class_weight)
+    clf = DecisionTreeClassifier(class_weight=class_weight)
     clf.fit(X_train, y_train)
     y_pred_train = clf.predict(X_train)
     y_pred_test = clf.predict(X_test_final)
